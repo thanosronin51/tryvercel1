d80eadb3592d9f8368faf74bbefb7583b56bf6c0
thanosronin51 <88163214+thanosronin51@users.noreply.github.com>
2023-07-19 22:28:43 +0100

    Update settings.py

--- project/settings.py
+++ project/settings.py
@@ -79,10 +79,15 @@ WSGI_APPLICATION = 'project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'Firstwork51a51$',
+        'HOST': 'db.qacrvqsjwfxtavcpwxjl.supabase.co',
+        'PORT': '6543',
     }
 }
+
 
 
 
